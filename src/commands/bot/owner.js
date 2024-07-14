cdaf4e55029eb80a9a3a84d434dd1165ea473972
Yabukimomo30 <yabukimomo30@gmail.com>
2024-07-14 20:13:41 +0800

    Update owner.js

--- src/commands/bot/owner.js
+++ src/commands/bot/owner.js
@@ -7,22 +7,22 @@ module.exports = async (client, interaction, args) => {
         thumbnail: client.user.avatarURL({ dynamic: true, size: 1024 }),
         fields: [{
             name: "👑┆Owner name",
-            value: `Corwin`,
+            value: `Earl`,
             inline: true,
         },
         {
             name: "🏷┆Discord tag",
-            value: `</Corwin>#0001`,
+            value: `Jarlssen`,
             inline: true,
         },
         {
             name: "🏢┆Organization",
-            value: `CoreWare`,
+            value: `The Universe`,
             inline: true,
         },
         {
             name: "🌐┆Website",
-            value: `[https://corwindev.nl](https://corwindev.nl)`,
+            value: `[My website](https://guns.lol/unibypasser)`,
             inline: true,
         }],
         type: 'editreply'
